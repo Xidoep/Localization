a6ebb13fbae1e699cb37849b11ba5d063080ab74
xeviegido <Xidostudio@gmail.com>
2023-03-08 17:11:20 +0100

    saved variable

--- Runtime/Scripts/Localitzacio.cs
+++ Runtime/Scripts/Localitzacio.cs
@@ -8,159 +8,110 @@ using XS_Utils;
 [CreateAssetMenu(menuName = "Xido Studio/Localitzacio/Localitzacio")]
 public class Localitzacio : ScriptableObject
 {
-    const string IDIOMA_ACTUAL_GUARDAT_KEY = "IdiomaActual";
+    const string KEY_IDIOMA_ACTUAL = "IdiomaActual";
 
-    public string IdiomaActualKey => IDIOMA_ACTUAL_GUARDAT_KEY;
-
-    public Guardat guardat;
+    [SerializeField] SavableVariable<int> idiomaSeleccionat;
     public Locale perDefecte;
-
 
 
     private void OnEnable()
     {
-        Debugar.Log("[Localitzacio] OnEnable => Carregar()");
-        //Carregar();
-        //This can not be loaded on Enable because the locales aren't initialized.
-        guardat.onLoad += Carregar;
-    }
-    private void OnDisable()
-    {
-        guardat.onLoad -= Carregar;
+        idiomaSeleccionat = new SavableVariable<int>(KEY_IDIOMA_ACTUAL, Guardat.Direccio.Cloud, IdiomaDelSistema());
+        IdiomaActual(idiomaSeleccionat.Valor);
     }
 
 
 
-    [ContextMenu("RecuperarIdiomaGuardat")]
-    public void Carregar()
+    public void IdiomaActual(int idioma)
     {
-        //CARREGAR el guardat abans de fer aixo, si no, no rebras res.
-        Debugar.Log($"[Localitzacio] RecuperarIdiomaGuardat { (int)guardat.Get(IdiomaActualKey, -1)}");
-        if(guardat != null) IdiomaActual((int)guardat.Get(IdiomaActualKey, -1));
-    }
-
-    public void IdiomaActual(int _locale)
-    {
-        //Si _locale es -1, es que no s'ha guardat cap informació sobre l'idiome escollit, 
-        //per tant ho gestiona el Localitzation (que deu agafar l'idioma del dispositiu).
-        if (_locale == -1)
+        if (idioma == -1 || idioma > LocalizationSettings.AvailableLocales.Locales.Count)
         {
             PerDefecte();
             return;
         }
 
-        //Per si de cas es selecciona un index que supera el rang, agafa l'idioma per defecte.
-        if (_locale > LocalizationSettings.AvailableLocales.Locales.Count)
-        {
-            PerDefecte();
-            return;
-        }
-
-        //Selecciona l'idioma amb l'index passat.
-        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[_locale];
-
-        //Guarda l'index a idiomes. Ja no utilitzarà més l'idioma del dispositiu.
-        if (guardat != null) guardat.SetLocal(IdiomaActualKey, _locale);
-
-        //¡¡¡GUARDAR el guardat despres de fer això!!!
+        SetIdioma(idioma);
     }
+    public void PerDefecte() => SetIdioma(-1);
+    public void PerDevice() => SetIdioma(IdiomaDelSistema());
 
 
-    public void PerDefecte()
+
+    void SetIdioma(int idioma)
     {
-        LocalizationSettings.SelectedLocale = perDefecte;
-        if (guardat != null) guardat.SetLocal(IdiomaActualKey, -1);
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[idioma];
+        idiomaSeleccionat.Valor = idioma;
     }
-
-    public void PerDevice()
+    int IdiomaDelSistema()
     {
-        int language = -1;
-        switch (Application.systemLanguage)
+        return Application.systemLanguage switch
         {
-            case SystemLanguage.Afrikaans:
-            case SystemLanguage.Arabic:
-            case SystemLanguage.Basque:
-            case SystemLanguage.Belarusian:
-            case SystemLanguage.Bulgarian:
-                language = -1;
-                break;
-            case SystemLanguage.Catalan:
-                language = 7;
-                break;
-            case SystemLanguage.Chinese:
-                language = 1;
-                break;
-            case SystemLanguage.Czech:
-            case SystemLanguage.Danish:
-            case SystemLanguage.Dutch:
-                language = -1;
-                break;
-            case SystemLanguage.English:
-                language = 0;
-                break;
-            case SystemLanguage.Estonian:
-            case SystemLanguage.Faroese:
-            case SystemLanguage.Finnish:
-            case SystemLanguage.French:
-                language = 6;
-                break;
-            case SystemLanguage.German:
-                language = 4;
-                break;
-            case SystemLanguage.Greek:
-            case SystemLanguage.Hebrew:
-            case SystemLanguage.Hungarian:
-            case SystemLanguage.Icelandic:
-            case SystemLanguage.Indonesian:
-            case SystemLanguage.Italian:
-            case SystemLanguage.Japanese:
-            case SystemLanguage.Korean:
-            case SystemLanguage.Latvian:
-            case SystemLanguage.Lithuanian:
-            case SystemLanguage.Norwegian:
-                language = -1;
-                break;
-            case SystemLanguage.Polish:
-                language = 8;
-                break;
-            case SystemLanguage.Portuguese:
-                language = 5;
-                break;
-            case SystemLanguage.Romanian:
-                language = -1;
-                break;
-            case SystemLanguage.Russian:
-                language = 2;
-                break;
-            case SystemLanguage.SerboCroatian:
-            case SystemLanguage.Slovak:
-            case SystemLanguage.Slovenian:
-                language = -1;
-                break;
-            case SystemLanguage.Spanish:
-                language = 3;
-                break;
-            case SystemLanguage.Swedish:
-            case SystemLanguage.Thai:
-            case SystemLanguage.Turkish:
-            case SystemLanguage.Ukrainian:
-            case SystemLanguage.Vietnamese:
-            case SystemLanguage.ChineseSimplified:
-            case SystemLanguage.ChineseTraditional:
-            case SystemLanguage.Unknown:
-                language = -1;
-                break;
-            default:
-                language = -1;
-                break;
-        }
-
-        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[language];
-        if (guardat != null) guardat.SetLocal(IdiomaActualKey, language);
+            SystemLanguage.Afrikaans or 
+            SystemLanguage.Arabic or 
+            SystemLanguage.Basque or 
+            SystemLanguage.Belarusian or 
+            SystemLanguage.Bulgarian 
+            => -1,
+            SystemLanguage.Catalan 
+            => 7,
+            SystemLanguage.Chinese 
+            => 1,
+            SystemLanguage.Czech or 
+            SystemLanguage.Danish or 
+            SystemLanguage.Dutch 
+            => -1,
+            SystemLanguage.English 
+            => 0,
+            SystemLanguage.Estonian or 
+            SystemLanguage.Faroese or 
+            SystemLanguage.Finnish or 
+            SystemLanguage.French 
+            => 6,
+            SystemLanguage.German 
+            => 4,
+            SystemLanguage.Greek or 
+            SystemLanguage.Hebrew or 
+            SystemLanguage.Hungarian or 
+            SystemLanguage.Icelandic or 
+            SystemLanguage.Indonesian or 
+            SystemLanguage.Italian or 
+            SystemLanguage.Japanese or 
+            SystemLanguage.Korean or 
+            SystemLanguage.Latvian or 
+            SystemLanguage.Lithuanian or 
+            SystemLanguage.Norwegian 
+            => -1,
+            SystemLanguage.Polish 
+            => 8,
+            SystemLanguage.Portuguese 
+            => 5,
+            SystemLanguage.Romanian 
+            => -1,
+            SystemLanguage.Russian 
+            => 2,
+            SystemLanguage.SerboCroatian or 
+            SystemLanguage.Slovak or 
+            SystemLanguage.Slovenian 
+            => -1,
+            SystemLanguage.Spanish 
+            => 3,
+            SystemLanguage.Swedish or 
+            SystemLanguage.Thai or 
+            SystemLanguage.Turkish or 
+            SystemLanguage.Ukrainian or 
+            SystemLanguage.Vietnamese or 
+            SystemLanguage.ChineseSimplified or 
+            SystemLanguage.ChineseTraditional or 
+            SystemLanguage.Unknown 
+            => -1,
+            _ => -1,
+        };
     }
+
+
 
     private void OnValidate()
     {
-        guardat = XS_Editor.LoadGuardat<Guardat>();
+        idiomaSeleccionat = new SavableVariable<int>(KEY_IDIOMA_ACTUAL, Guardat.Direccio.Cloud, IdiomaDelSistema());
     }
 }
